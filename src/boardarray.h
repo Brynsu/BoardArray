27373d5d76b8a08290869a18c80462a4c4c365f3
Luke Brynson Caneda <lukebrynson@gmail.com>
2025-09-22 22:42:26 +0800

    implemented function

--- src/boardarray.h
+++ src/boardarray.h
@@ -16,7 +16,44 @@ class BoardArray : public Board {
         void add(Entry* entry) {
             // TODO: IMPLEMENT THIS FUNCTION
             // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
+            if (index == 0) {
+            array[0] = *entry;
+            index++;
+            return;
+            }
 
+            int pos = -1;
+            for (int i = 0; i < index; i++) {
+                if (entry->compare(&array[i])) {
+                pos = i;
+                break;
+                }
+            }
+
+            if (pos == -1) {
+                if (index < SIZE) {
+                    array[index] = *entry;
+                    index++;
+                } else {
+                    cout << entry->name << "'s score is too low to be added!" << endl;
+                }
+                return;
+            }   
+
+            if (index == SIZE) {
+                if (!entry->compare(&array[index - 1])) {
+                    cout << entry->name << "'s score is too low to be added!" << endl;
+                    return;
+                }
+            } else {
+                index++;
+            }
+
+            for (int j = index - 1; j > pos; j--) {
+                array[j] = array[j - 1];
+            }
+
+             array[pos] = *entry;
         }
 
         void print() {
